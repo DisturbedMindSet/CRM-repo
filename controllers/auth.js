d62494ec59a2645bd4fb3005c01a62ce8ce9148d
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-09 10:49:56 +0100

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,20 +6,20 @@ exports.register = (req, resp) => {
 
 	const { name, email, password, passwordConfirm } = req.body;
 
-	// db.query("SELECT email FROM users WHERE email =?", [email], (error, results) => {
-	// 	if (error) {
-	// 		console.log(error);
-	// 	}
-	// 	if (results.length > 0) {
-	// 		return res.render("user", {
-	// 			message: "that email is already in use",
-	// 		});
-	// 	} else if (password !== passwordConfirm) {
-	// 		return res.render("user", {
-	// 			message: "the password do not match",
-	// 		});
-	// 	}
-	// });
+	db.query("SELECT email FROM users WHERE email =?", [email], (error, results) => {
+		if (error) {
+			console.log(error);
+		}
+		if (results.length > 0) {
+			return res.render("index", {
+				message: "that email is already in use",
+			});
+		} else if (password !== passwordConfirm) {
+			return res.render("index", {
+				message: "the password do not match",
+			});
+		}
+	});
 
 	resp.send("form submitted");
 };
