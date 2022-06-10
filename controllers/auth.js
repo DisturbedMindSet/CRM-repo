d24425a6a9a466c0475c85e5b97852cfde9d2080
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-10 12:17:01 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,25 +6,25 @@ exports.register = (req, res) => {
 
 	const { name, email, password, passwordConfirm } = req.body;
 
-	db.query("SELECT email FROM users WHERE email =?", [email], (error, results) => {
-		if (error) {
-			console.log(error);
+	// db.query("SELECT email FROM users WHERE email =?", [email], (error, results) => {
+	// 	if (error) {
+	// 		console.log(error);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		// console.log(Object.keys(results));
-		if (Object.keys(results).length > 0) {
-			return res.render("register", { message: "that email is already in use" });
-		}
-		// // 	console.dir(res.headersSent);
-		// } else if (password !== passwordConfirm) {
-		// 	return res.render("test", {
-		// 		message: "the password do not match",
-		// 	});
-		// 	console.dir(res.headersSent);
-		// }
-	});
+	// 	// console.log(Object.keys(results));
+	// 	if (Object.keys(results).length > 0) {
+	// 		return res.render("register", { message: "that email is already in use" });
+	// 	}
+	// 	// // 	console.dir(res.headersSent);
+	// 	// } else if (password !== passwordConfirm) {
+	// 	// 	return res.render("test", {
+	// 	// 		message: "the password do not match",
+	// 	// 	});
+	// 	// 	console.dir(res.headersSent);
+	// 	// }
+	// });
 
 	// db.query('SELECT * FROM user WHERE id = "1"', (error, rows) => {
 	// 	if (error) {
