19fe80dc3aa610c9b0618b557a6c960fb0d509e0
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-12 00:15:11 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const conn = {
-	connectionLimit: 10,
+	connectionLimit: 100,
 	host: process.env.DATABASE_HOST,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
