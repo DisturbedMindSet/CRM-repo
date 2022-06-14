5c536c890f7595add988024332d586ec4475d2e9
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-14 23:39:05 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,55 +1,35 @@
 const mysql = require("mysql");
 
-// const conn = {
-// 	connectionLimit: 10,
-// 	host: process.env.DATABASE_HOST,
-// 	user: process.env.DATABASE_USER,
-// 	password: process.env.DATABASE_PASSWORD,
-// 	database: process.env.DATABASE,
-// };
+const conn = {
+	connectionLimit: 10,
+	host: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE,
+};
 
-// function handleDisconnect() {
-// 	db = mysql.createConnection(conn);
+function handleDisconnect() {
+	db = mysql.createConnection(conn);
 
-// 	db.connect(function (err) {
-// 		if (err) {
-// 			console.log("error when connecting to db:", err);
+	db.connect(function (err) {
+		if (err) {
+			console.log("error when connecting to db:", err);
 
-// 			setTimeout(handleDisconnect, 2000);
-// 		} else {
-// 			console.log("successFull connection");
-// 		}
-// 	});
-// 	db.on("error", function (err) {
-// 		console.log("db error", err);
+			setTimeout(handleDisconnect, 2000);
+		} else {
+			console.log("successFull connection");
+		}
+	});
+	db.on("error", function (err) {
+		console.log("db error", err);
 
-// 		if (err.code === "PROTOCOL_CONNECTION_LOST") {
-// 			handleDisconnect();
-// 		} else {
-// 			throw err;
-// 		}
-// 	});
-// }
-// handleDisconnect();
-
-
-
-let sequelize;
-
-if (process.env.CLEARDB_DATABASE_URL) {
-  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
+		if (err.code === "PROTOCOL_CONNECTION_LOST") {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
 }
-
+handleDisconnect();
 
 module.exports = db;
