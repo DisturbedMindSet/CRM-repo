6c401b7bee45749fbc2ce783ca96ec24ca0638f7
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-23 22:09:41 +0100

    Create server.js

---
+++ src/server/server.js
@@ -0,0 +1,41 @@
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import logging from "./config/logging";
+import config from "./config/config";
+
+import cors from "cors";
+
+const NAMESPACE = "server";
+const router = express();
+
+router.use((req, res, next) => {
+	logging.info(
+		NAMESPACE,
+		`METHOD - [${req.method}],
+       URL - [${req.url}], 
+       IP - [${req.socket.remoteAddress}]`,
+	);
+
+	res.on("finish", () => {
+		logging.info(
+			NAMESPACE,
+			`METHOD - [${req.method}],
+          URL - [${req.url}], 
+          IP - [${req.socket.remoteAddress}]
+          STATUS - [${res.statusCode}]`,
+		);
+	});
+});
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.use(bodyParser.json());
+
+router.use((req,res,next) =>{
+   res.header("Access-Control-Allow-origin", "http://localhost:4000/") //mudar para o IP ou criar variavel em config
+
+   
+})
+
+
