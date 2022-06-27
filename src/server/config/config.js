4332153916738fd6f35583fefc51c50b2202be5c
DisturbedMindSet <67000500+DisturbedMindSet@users.noreply.github.com>
2022-06-27 10:00:04 +0100

    estruturar package.json
    + criacao de mongoDB

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -2,6 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const MONGO_OPTIONS = {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	keepAlive: true,
+	socketTimeoutMS: 30000,
+	poolSize: 50,
+	autoIndex: false,
+	retryWrites: false,
+};
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
 const SERVER_PORT = process.env.PORT || 4000;
 
