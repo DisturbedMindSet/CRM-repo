2d0d9a7f07ada71713143388a4d4792908eca8cd
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-08-09 23:02:28 +0100

    Update error.js

--- Middleware/error.js
+++ Middleware/error.js
@@ -3,23 +3,25 @@ const errorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
 	let error = { ...err };
 
+	console.log(err.code);
+
 	error.message = err.message;
 
 	// error on mongoose
 	if (err.code === 11000) {
 		const message = "Duplicate Field Value Enter";
-		error = new errorResponse(message, 400);
+		error = new errorResponse(message, 500);
 	}
 
 	// error on mongoose
 	if (error.name === "ValidationError") {
-		const message = Object.values(err.errors).map(val => val.message);
+		const message = Object.values(err.errors).map((val) => val.message);
 
 		error = new errorResponse(message, 400);
 	}
 
 	res.status(error.statusCode || 500).json({
-		sucess: false,
+		success: false,
 		error: error.message || "Server Error",
 	});
 };
