0eb40b86e710242263d5fecfebd1eba58d43461c
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-08-18 16:09:38 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { refreshToken } = require("../controllers/refreshTokenController");
 
 const { register, login, forgotPassword, resetPassword } = require("../controllers/auth");
 
 router.route("/register").post(register);
 
 router.route("/login").post(login);
+router.route("/refresh").post(refreshToken);
 
 router.route("/forgotpassword").post(forgotPassword);
 
