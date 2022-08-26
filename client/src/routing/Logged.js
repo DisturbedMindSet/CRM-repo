fe3b487cfd9a19df55b051a1d3e12352f4b7c59a
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-08-26 14:35:31 +0100

    Create Logged.js
    test

---
+++ client/src/routing/Logged.js
@@ -0,0 +1,29 @@
+// import { useState } from "react";
+
+// const Logged = () => {
+// 	const [authed, setAuthed] = useState(false);
+
+// 	useEffect(() => {
+// 		if (token === null) {
+// 			return navigate("/login");
+// 		}
+// 	});
+
+// 	return {
+// 		authed,
+// 		login() {
+// 			return new Promise((res) => {
+// 				setAuthed(true);
+// 				res();
+// 			});
+// 		},
+// 		logout() {
+// 			return new Promise((res) => {
+// 				setAuthed(false);
+// 				res();
+// 			});
+// 		},
+// 	};
+// };
+
+// export default Logged;
