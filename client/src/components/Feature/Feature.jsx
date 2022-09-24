3bbbe02c6417b3df4c305bc7dd7e41818a25cfb8
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-09-24 10:25:11 +0100

    Create Feature.jsx

---
+++ client/src/components/Feature/Feature.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Feature.scss";
+
+const Feature = () => {
+	return (
+		<section className="feature">
+			<div>Feature</div>
+		</section>
+	);
+};
+
+export default Feature;
