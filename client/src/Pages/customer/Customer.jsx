8c6e20482017a3399c3ffefe06a89fc3e932a9bc
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-09-24 10:25:47 +0100

    Create Customer.jsx

---
+++ client/src/Pages/customer/Customer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+const Customer = () => {
+	return (
+		<section className="list">
+			<Sidebar />
+			<div className="listContainer">
+				<Navbar />
+				datatable
+			</div>
+		</section>
+	);
+};
+
+export default Customer;
