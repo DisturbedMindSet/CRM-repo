e9f9b3821d3cc54e668cee53439283d01e659830
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-09-24 10:26:21 +0100

    Create Main.jsx

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,29 @@
+// import React from "react";
+// // import Footer from "./test/footer/Footer";
+// // import Navigation from "./Navbar/Navigation";
+
+// import Sidebar from "./Sidebar/Sidebar";
+// import "./Main.scss";
+// import Navbar from "./Navbar/Navbar";
+// import Widget from "./Widget/Widget";
+
+// const MainHome = () => {
+// 	return (
+// 		<React.Fragment>
+// 			<div id="Home" className="d-flex ">
+// 				<Sidebar />
+// 				<main id="container" className="">
+// 					<Navbar />
+// 					<section className="widgets">
+// 						<Widget type="customer" />
+// 						<Widget type="order" />
+// 						<Widget type="earning" />
+// 						<Widget type="balance" />
+// 					</section>
+// 				</main>
+// 			</div>
+// 		</React.Fragment>
+// 	);
+// };
+
+// export default MainHome;
