a786a0cb5792452468e77703bda4ee0d8a6106fe
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2022-09-24 10:24:59 +0100

    Create Chart.jsx

---
+++ client/src/components/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chart.scss";
+
+const Chart = () => {
+	return (
+		<section className="chart">
+			<div>Chart</div>
+		</section>
+	);
+};
+
+export default Chart;
